a08828916f455cdf0ed1fc33cd802c6fe845ef84
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2023-06-12 16:50:36 -0700

    Layout improvements

--- src/main/java/us/malfeasant/notecalc/App.java
+++ src/main/java/us/malfeasant/notecalc/App.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.NumberBinding;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -32,12 +34,22 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         var pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setVgap(5);
+        pane.setHgap(5);
+        pane.setPadding(new Insets(5));
+
         int i = 0;
         for (var d : Denominations.values()) {
             var value = d.value;
             var label = new Label("$" + value);
+            label.setMinWidth(30);
             var count = new TextField("0");
+            count.setMaxWidth(70);
+            count.setMinWidth(60);
+            count.setPrefWidth(50);
             var amount = new Label();
+            amount.setMinWidth(50);
             var intermediate = 
                 Bindings.multiply(value, 
                 Bindings.createIntegerBinding(() -> {
@@ -49,26 +61,32 @@ public class App extends Application {
                         sb.append(matcher.group());
                     }
                     // if no valid numbers were found, string will be empty at this point
-                    if (sb.isEmpty()) sb.append("0");
+                    if (sb.isEmpty()) {
+                        sb.append("0");
+                    }
+                    // Limit to 5 characters
+                    if (sb.length() > 5) {
+                        sb.setLength(5);
+                    }
                     // replace the text in the input with the number found
                     count.textProperty().set(sb.toString());
                     return Integer.parseInt(count.getText());   // shouldn't throw exception...
                 }, count.textProperty()));
             amount.textProperty().bind(Bindings.convert(intermediate));
 
-            pane.add(label, 0, i);
-            pane.add(count, 1, i);
-            pane.add(amount, 2, i);
+            pane.add(label, 0, i, 1, 1);
+            pane.add(count, 1, i, 2, 1);
+            pane.add(amount, 3, i, 1, 1);
             
             if (result == null) result = intermediate;  // first one
             else result = Bindings.add(result, intermediate);
             ++i;
         }
 
-        pane.add(new Label("Total: "), 0, i);
+        pane.add(new Label("Total: "), 0, i, 2, 1);
         var resultLabel = new Label();
         resultLabel.textProperty().bind(result.asString());
-        pane.add(resultLabel, 2, i);
+        pane.add(resultLabel, 3, i);
 
         stage.setScene(new Scene(pane));
         stage.show();
